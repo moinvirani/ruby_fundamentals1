74f4d0ff1c0fe7fe67851a86744555db131872bb
Moin Virani <moin.virani@gmail.com>
2013-10-21 23:59:53 -0400

    Adding age of user to Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,11 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+puts "How old are you #{name}?"
+
+age = gets.chomp.to_i # convert string to float
+
+year = 2013-age
+
+puts "You were born in #{year}"
